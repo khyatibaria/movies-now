f9241b93e6ff6677faf7ee43b24c55ac9f654327
Khyati Baria <khyatibaria55@gmail.com>
2022-09-14 00:06:19 +0530

    feat movie rows

---
+++ src/components/Row.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import "./Row.css";
+import instance from "../services";
+const Row = (props) => {
+  const { title, api, isPosterLarge = false } = props;
+  const [allPosters, setAllPosters] = useState([]);
+
+  useEffect(() => {
+    async function getPosters(api) {
+      const response = await instance.get(api);
+      const result = response.data.results;
+      console.log(result);
+      setAllPosters(result);
+    }
+    getPosters(api);
+  }, []);
+  return (
+    <div className="row">
+      <div className="row_title">{title}</div>
+      <div className="row_poster_container">
+        {allPosters?.map((poster) => {
+          return (
+            <img
+              key={poster.id}
+              src={`http://image.tmdb.org/t/p/original/${
+                isPosterLarge ? poster.poster_path : poster.backdrop_path
+              }`}
+              className={isPosterLarge ? "row_largeposter" : "row_poster"}
+              alt={poster.name}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default Row;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,25 @@
 import "./Home.css";
 import Nav from "../components/Nav";
 import Banner from "../components/Banner";
+import Row from "../components/Row";
+import { getMovies } from "../services/getMovies";
 const Home = () => {
   return (
-    <div style={{ height: "200vh" }}>
+    <div>
       <Nav />
       <Banner />
+      <div style={{ minHeight: "50vh", backgroundColor: "#030d45" }}>
+        <Row
+          title="Trending Now"
+          api={getMovies.trending}
+          isPosterLarge={true}
+        />
+        <Row title="Drama" api={getMovies.drama} />
+        <Row title="Romance" api={getMovies.romance} />
+        <Row title="Comdey" api={getMovies.comedy} />
+        <Row title="Action" api={getMovies.action} />
+        <Row title="Horror" api={getMovies.horror} />
+      </div>
     </div>
   );
 };
