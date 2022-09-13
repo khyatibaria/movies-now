b2b6f3cf56483485170cb240c0d2f5719bd7719f
Khyati Baria <khyatibaria55@gmail.com>
2022-09-13 22:36:43 +0530

    feat banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,37 +1,40 @@
 import { useEffect, useState } from "react";
 import "./Banner.css";
-import axios from "../services/index";
+import instance from "../services/index";
 import { getMovies } from "../services/getMovies";
 
 const Banner = (props) => {
   const [banner, setBanner] = useState(null);
+  const image_key = "http://image.tmdb.org/t/p/";
 
   useEffect(() => {
-    async function fetchMovies() {
-      const result = await axios.get(getMovies.action);
-      setBanner();
-      console.log(result);
-      return result;
+    async function getBanner(api) {
+      const response = await instance.get(api);
+      const result = response.data.results;
+      const randomBanner = Math.ceil(Math.random() * result.length);
+      console.log(result[randomBanner]);
+      setBanner(result[randomBanner]);
     }
-    fetchMovies();
+    getBanner(getMovies.trending);
   }, []);
-  //   fetch()
   return (
     <div
       className="banner"
       style={{
-        backgroundImage: `url(${"https://i.pinimg.com/474x/a7/6d/db/a76ddb0c7760bc31c36aae3206876676.jpg"})`,
-        backgroundPosition: "center center",
+        backgroundImage: `url(http://image.tmdb.org/t/p/original/${banner?.backdrop_path})`,
+        // backgroundPosition: "center center",
         backgroundSize: "cover",
       }}
     >
       <div className="banner_contents">
-        <div className="banner_title">movie</div>
+        <div className="banner_title">
+          {banner?.title || banner?.original_title}
+        </div>
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <div className="banner_discription">discription</div>
+        <div className="banner_discription">{banner?.overview}</div>
       </div>
     </div>
   );
