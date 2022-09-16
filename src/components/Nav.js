3f39b19e1afcda1c0e113fc3b2e8e2aa1d50c668
Khyati Baria <khyatibaria55@gmail.com>
2022-09-16 15:37:59 +0530

    refactor nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,11 @@
 import "./Nav.css";
 import { images } from "../asset/index";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Nav = (props) => {
   const { DarkLogo, DarkAvatar, LightLogo, LightAvatar } = images;
   const [navDark, setNavDark] = useState(false);
+  let navigate = useNavigate();
   const scrollEventHandler = () => {
     if (window.scrollY > 100) {
       setNavDark(true);
@@ -26,6 +28,9 @@ const Nav = (props) => {
             src={navDark ? LightLogo : DarkLogo}
             alt="logo"
             style={{ color: "white" }}
+            onClick={() => {
+              navigate("/");
+            }}
           />
           <h1 className={navDark ? "brand_light" : "brand_dark"}>Movies Now</h1>
         </div>
@@ -34,6 +39,9 @@ const Nav = (props) => {
             className="nav_avatar"
             src={navDark ? LightAvatar : DarkAvatar}
             alt="avatar"
+            onClick={() => {
+              navigate("/profile");
+            }}
           />
         </div>
       </div>
